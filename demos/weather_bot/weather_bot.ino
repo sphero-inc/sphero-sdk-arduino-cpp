de71e7d4619e8a94d93341f4b4ec594ac038c779
Megan Robinson <megan.robinson@sphero.com>
2019-11-12 14:52:10 -0700

    Added comments to .ino files

--- demos/weather_bot/weather_bot.ino
+++ demos/weather_bot/weather_bot.ino
@@ -1,34 +1,53 @@
 #include "DHT.h"
 #include <SpheroRVR.h>
 
+
+// this is the pin connected to the data pin of the sensor
 #define DHTPIN 4
+
+// this is the type of sensor (you'll need to change this if you're using a different sensor!)
 #define DHTTYPE DHT22
+
+// this is a bitmask that indicates to the RVR that we want to set all the LEDs
 #define ALL_LEDS 0x3FFFFFFF
 
+
+// instantiate temperature-humidity sensor object
 DHT dht(DHTPIN, DHTTYPE);
 
+
 void setup() {
+  // turn temperature-humidity sensor on
   dht.begin();
 
+  // set up communication with the RVR
   rvr.configUART(&Serial);
 
+  // give RVR time to wake up
   delay(2000);
 }
 
 void loop() {
+  // loop every 2 seconds
   delay(2000);
-  
+
+  // read the temperature from the sensor in Fahrenheit
   float temperature = dht.readTemperature(true);
 
+  // if we get a bad reading, continue to next loop iteration
   if (isnan(temperature)) {
     return;
   }
-  
+
+  // translate a temperature ranging from 0F-100F to a red or blue value ranging from 0-255
+  // note: you'll need to adjust the math if you're exposing your RVR to temperatures below 0F or above 100F
   uint8_t redValue = static_cast<uint8_t>(temperature / 100 * 255);
   uint8_t blueValue = static_cast<uint8_t>((100 - temperature) / 100 * 255);
 
+  // we're setting 10 LEDs that each need 3 values (red, green, blue), so we need an array of size 30
   uint8_t ledValues[30] = {0};
 
+  // populate ledValues array so that every LED will be the same color: (redValue, 0, blueValue)
   for (int i = 0; i < 28; i = i + 3)
   {
     ledValues[i] = redValue;
@@ -36,5 +55,6 @@ void loop() {
     ledValues[i + 2] = blueValue;
   }
 
+  // tell RVR to update the LED colors
   rvr.setAllLeds(ALL_LEDS, ledValues, 30);
 }
--- demos/weather_bot/weather_bot_display.ino
+++ demos/weather_bot/weather_bot_display.ino
@@ -3,9 +3,14 @@
 #include "Adafruit_LEDBackpack.h"
 #include "DHT.h"
 
+
+// this is the pin connected to the data pin of the sensor
 #define DHTPIN  12
+
+// this is the type of sensor (you'll need to change this if you're using a different sensor!)
 #define DHTTYPE DHT22
 
+// these are bitmasks that tell the display which segments need to be turned on for each character
 #define ZERO    0x0C3F
 #define ONE     0x0006
 #define TWO     0x00DB
@@ -19,62 +24,83 @@
 #define DECIMAL 0x4000
 #define OFF     0x0000
 
+
+// instantiate alphanumeric display object
 Adafruit_AlphaNum4 output = Adafruit_AlphaNum4();
 
+// instantiate temperature-humidity sensor object
 DHT dht(DHTPIN, DHTTYPE);
 
+
 void setup() {
+  // turn alphanumeric display on
   output.begin(0x70);
+  // turn temperature-humidity sensor on
   dht.begin();
 
+  // turn all segments of the display off
   output.writeDigitRaw(0, OFF);
   output.writeDigitRaw(1, OFF);
   output.writeDigitRaw(2, OFF);
   output.writeDigitRaw(3, OFF);
   output.writeDisplay();
 
+  // give RVR time to wake up
   delay(2000);
 }
 
 void loop() {
+  // loop every 2 seconds
   delay(2000);
 
+  // read the temperature from the sensor in Fahrenheit
   float temperature = dht.readTemperature(true);
 
+  // if we get a bad reading, continue to next loop iteration
   if (isnan(temperature)) {
     return;
   }
 
+  // convert the temperature float to a char array
   char temperatureBuffer[5];
   dtostrf(temperature, 5, 2, temperatureBuffer);
 
+  // loop through temperature char array
   for (int i = 0; i < 5; i++)
   {
+    // skip the decimal point
     if (i == 2)
     {
       continue;
     }
 
+    // set the display index
     int index = i;
 
+    // offset the display index by 1 if we're past the decimal point in the char array
     if (i > 2)
     {
       index = i - 1;
     }
 
+    // get the bitmask for the current digit
     int digit = getDigit(temperatureBuffer[i]);
 
+    // add a decimal point after the second digit
     if (i == 1)
     {
       digit |= DECIMAL;
     }
 
+    // write digit to the given index of the display
     output.writeDigitRaw(index, digit);
   }
 
+  // update display
   output.writeDisplay();
 }
 
+// convert a char to its corresponding digit bitmask
 int getDigit(char digit) {
   switch (digit) {
     case '0':
